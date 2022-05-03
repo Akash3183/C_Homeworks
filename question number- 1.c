e8b4080b261fa2dad45ffe4b8aa3747e7d94dddd
Akash3183 <104429324+Akash3183@users.noreply.github.com>
2022-05-03 18:51:52 +0530

    Add files via upload

---
+++ question number- 1.c
@@ -0,0 +1,11 @@
+//write a C program to print your name, date of birth and mobile number.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Enter name: Akash Bhattacharjee\n");
+    printf("Enter date of birth: 07/12/2002\n");
+    printf("Enter mobile number: 8697040670\n");
+    return 0;
+}
